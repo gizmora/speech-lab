75e7a03459f11084546de9df7fded416dc929b9c
Grace Mora <50357533+gizmora@users.noreply.github.com>
2025-07-27 13:50:30 +0800

    chore: convert to scss

--- src/app/pages/view-speech/view-speech.ts
+++ src/app/pages/view-speech/view-speech.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderNav } from '../../shared/components/header-nav/header-nav';
 
 @Component({
   selector: 'app-view-speech',
-  imports: [],
+  imports: [HeaderNav],
   templateUrl: './view-speech.html',
-  styleUrl: './view-speech.css'
+  styleUrl: './view-speech.scss'
 })
 export class ViewSpeech {
 
